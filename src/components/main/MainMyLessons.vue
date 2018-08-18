79644947ba21065178317844e81e5e2a7b52a6e1
cengxiaogang <zengxiaogang@jd.com>
2018-08-19 02:24:19 +0800

    标明未来开发计划

--- src/components/main/MainMyLessons.vue
+++ src/components/main/MainMyLessons.vue
@@ -1,11 +1,11 @@
 <template>
-
+  <h1>暂无开发计划。。。</h1>
 </template>
 
 <script>
-    export default {
-        name: "MainMyLessons"
-    }
+  export default {
+    name: "MainMyLessons"
+  }
 </script>
 
 <style scoped>
--- src/components/main/MainNextTermLessons.vue
+++ src/components/main/MainNextTermLessons.vue
@@ -1,5 +1,5 @@
 <template>
-
+  <h1>暂无开发计划。。。</h1>
 </template>
 
 <script>
